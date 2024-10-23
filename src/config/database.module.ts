fbf278eb1378d7ee8f9f63607a45a8f37964570e
gmendesfonseca <fonseca.gabriel.mendes@gmail.com>
2024-10-23 09:25:10 -0300

    feat :sparkles: add projects to database module

--- src/config/database.module.ts
+++ src/config/database.module.ts
@@ -5,6 +5,7 @@ import { User } from 'src/user/user.model';
 import { ConfigProperties } from './config.module';
 import { Company } from 'src/company/company.model';
 import { Person } from 'src/person/person.model';
+import { Project } from 'src/project/project.model';
 
 @Module({
   imports: [
@@ -12,7 +13,7 @@ import { Person } from 'src/person/person.model';
       imports: [ConfigModule],
       useFactory: (configService: ConfigService<ConfigProperties>) => {
         return {
-          models: [User, Company, Person],
+          models: [User, Company, Person, Project],
           logging: configService.get('DB_LOGGING'),
           autoLoadModels: configService.get('DB_AUTO_LOAD_MODELS'),
           dialect: 'postgres',
