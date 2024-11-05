c96dc8547cf8230cd7b6ed4f8a9629c6814c94ab
GABRIEL MENDES FONSECA <124696627+Gmendesfonseca@users.noreply.github.com>
2024-11-05 18:17:19 -0300

    fix :bug: change company relation

--- src/product/repositories/product.sequelize.repository.ts
+++ src/product/repositories/product.sequelize.repository.ts
@@ -1,30 +1,37 @@
-import { Injectable } from "@nestjs/common";
-import { Product } from "../product.model";
-import { ProductRepositoryInterface } from "../interfaces/product.repository.interface";
-import { InjectModel } from "@nestjs/sequelize";
+import { Injectable } from '@nestjs/common';
+import { Product } from '../product.model';
+import { ProductRepositoryInterface } from '../interfaces/product.repository.interface';
+import { InjectModel } from '@nestjs/sequelize';
 
 @Injectable()
-export class ProductSequelizeRepository implements ProductRepositoryInterface.ProductRepository {
-    constructor(@InjectModel(Product) private productModel: typeof Product) { }
+export class ProductSequelizeRepository
+  implements ProductRepositoryInterface.ProductRepository
+{
+  constructor(@InjectModel(Product) private productModel: typeof Product) {}
 
-    async findOne(id: string): Promise<Product> {
-        return await this.productModel.findOne({ where: { id } });
-    }
+  async findOne(id: string): Promise<Product> {
+    return await this.productModel.findOne({ where: { id } });
+  }
 
-    async findAll(companyId: string): Promise<Product[]> {
-        return await this.productModel.findAll({ where: { company_id: companyId } });
-    }
+  async findAll(companyId: string): Promise<Product[]> {
+    return await this.productModel.findAll({ where: { companyId: companyId } });
+  }
 
-    async create(payload: ProductRepositoryInterface.Inputs.payloadProduct): Promise<Product> {
-        return await this.productModel.create(payload);
-    }
+  async create(
+    payload: ProductRepositoryInterface.Inputs.payloadProduct,
+  ): Promise<Product> {
+    return await this.productModel.create(payload);
+  }
 
-    async update(product: Product, payload: ProductRepositoryInterface.Inputs.payloadProduct): Promise<Product> {
-        product.update(payload);
-        return await product.save();
-    }
+  async update(
+    product: Product,
+    payload: ProductRepositoryInterface.Inputs.payloadProduct,
+  ): Promise<Product> {
+    product.update(payload);
+    return await product.save();
+  }
 
-    async destroy(id: string): Promise<void> {
-        await this.productModel.destroy({ where: { id } });
-    }
+  async destroy(id: string): Promise<void> {
+    await this.productModel.destroy({ where: { id } });
+  }
 }
